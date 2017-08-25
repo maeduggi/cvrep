0fca7d598fbf5daf3ecc66a71ee6d8c457533bf5
robin posch <r.posch@posteo.de>
2017-08-25 10:31:10 +0200

    added OpenCV link stuff in CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
-// abstract base class
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-#include "ImageReader.h"
-#include "OpenCVImageReader.h"
-
+using namespace cv;
 using namespace std;
 
-int main() {
-	
+int main(int argc, char** argv)
+{
+	Mat image;
+	image = imread("myImage.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file
+
+	if (!image.data)                              // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+	}
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window", image);                   // Show our image inside it.
+
+	waitKey(0);                                          // Wait for a keystroke in the window
 	return 0;
 }
